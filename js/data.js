6beab3d206eb8312fdaa7dc0c2a70be13149a373
sogrey <suzhaoheng2006@163.com>
2026-05-09 23:33:28 +0800

    fix: 删除重复的 shader-tunnel 条目，恢复 shader-blackhole.html

--- js/data.js
+++ js/data.js
@@ -1163,22 +1163,6 @@ const threejsData = {
                             url: "examples/z258/shader-textStarShader"
                         },
                         {
-                            id: "shader-blackhole",
-                            title: "黑洞效果",
-                            description: "使用光线追踪模拟引力透镜效应和吸积盘",
-                            thumbnail: "images/shader-blackhole.jpg",
-                            version: "r179",
-                            url: "examples/z258/shader-blackhole"
-                        },
-                        {
-                            id: "shader-tunnel",
-                            title: "黑洞效果",
-                            description: "展示黑洞效果的着色器实现，使用光线追踪模拟引力透镜效应和吸积盘",
-                            thumbnail: "images/shader-blackhole.jpg",
-                            version: "r179",
-                            url: "examples/z258/shader-blackhole"
-                        },
-                        {
                             id: "shader-heartShader",
                             title: "心形着色器",
                             description: "展示心形曲线着色器效果，使用TubeGeometry创建心形并添加渐变色动画",
